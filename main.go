899cbafa59bd196f5079ebeb99b5e03714322a45
Julya <julya.meleshko@gmail.com>
2024-06-09 10:01:39 +0300

    Implement client

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"tgtest/clients/telegram"
+)
+
+const(
+	tgBotHost = "api.telegram.org"
+)
+
+
+func main() {
+	tgClient = telegram.New(mustToken())
+}
+
+func mustToken() string {
+	token := flag.String(
+		"token-bot-token", 
+		"",
+		"token for access telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
+}
